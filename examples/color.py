7005c1d677fff617a3a568df25130cee9456fc92
Kyle Kelley <kyle.kelley@rackspace.com>
2014-03-10 22:05:39 -0500

    Add color example

---
+++ examples/color.py
@@ -0,0 +1,14 @@
+def print_format_table():
+    """
+    prints table of formatted text format options
+    """
+    for style in xrange(8):
+        for fg in xrange(30,38):
+            s1 = ''
+            for bg in xrange(40,48):
+                format = ';'.join([str(style), str(fg), str(bg)])
+                s1 += '\x1b[%sm %s \x1b[0m' % (format, format)
+            print s1
+        print '\n'
+
+print_format_table()
