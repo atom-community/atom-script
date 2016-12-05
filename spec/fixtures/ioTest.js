066dab1a7c5187bf4ec9d5febb684b6eac5bc9cf
Lukas Geiger <lukas.geiger94@gmail.com>
2016-12-05 10:41:23 +0100

    Fix null comparisons

--- spec/fixtures/ioTest.js
+++ spec/fixtures/ioTest.js
@@ -2,7 +2,7 @@ process.stdin.setEncoding('utf8');
 
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
-  if (chunk !== null) {
+  if (chunk) {
     console.log(`TEST: ${chunk}`);
   }
 });
