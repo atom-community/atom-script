9f987c2ed69097133d0755ed31a4b5232a3a2969
Василий Чернов < >
2017-05-26 23:55:20 +0300

    Fix error stack links detecting for Windows paths. Add python error trace support. Add common files linking option.

--- spec/link-paths-spec.js
+++ spec/link-paths-spec.js
@@ -14,8 +14,8 @@ describe('linkPaths', () => {
   });
 
   it('detects file paths with Windows style drive prefix', () => {
-    const result = linkPaths('foo() C:/b/c.js:44:55');
-    expect(result).toContain('data-path="C:/b/c.js"');
+    const result = linkPaths('foo() C:\\b\\c.js:44:55');
+    expect(result).toContain('data-path="C:\\b\\c.js"');
   });
 
   it('allow ommitting the column number', () => {
@@ -24,10 +24,24 @@ describe('linkPaths', () => {
     expect(result).toContain('data-column=""');
   });
 
+  it('allow ommitting the column and line numbers', () => {
+    const result = linkPaths('foo() b/c.js');
+    expect(result).toContain('data-path="b/c.js"');
+    expect(result).toContain('data-line=""');
+    expect(result).toContain('data-column=""');
+  });
+
   it('links multiple paths', () => {
     const multilineResult = linkPaths('foo() b/c.js:44:5\nbar() b/c.js:45:56',
     );
     expect(multilineResult).toContain('foo() <a');
     expect(multilineResult).toContain('bar() <a');
+  });
+
+  it('detects python file paths with line numbers', () => {
+    const result = linkPaths('File "b/c.py", line 4, in <module>');
+    expect(result).toContain('data-path="b/c.py"');
+    expect(result).toContain('data-line="4"');
+    expect(result).toContain('data-column=""');
   });
 });
