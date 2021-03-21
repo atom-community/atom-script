b3078f7e0633ef893372b63771cc95e6b4bae9ef
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-03-21 11:08:19 -0500

    chore: format

--- spec/fixtures/ioTest.js
+++ spec/fixtures/ioTest.js
@@ -1,8 +1,8 @@
-process.stdin.setEncoding('utf8');
+process.stdin.setEncoding("utf8")
 
-process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
+process.stdin.on("readable", () => {
+  const chunk = process.stdin.read()
   if (chunk) {
-    console.log(`TEST: ${chunk}`);
+    console.log(`TEST: ${chunk}`)
   }
-});
+})
--- spec/runner-spec.js
+++ spec/runner-spec.js
@@ -1,89 +1,87 @@
-'use babel';
+"use babel" // TODO
 
-/* eslint-disable no-invalid-this */ // TODO
+/* eslint-disable no-invalid-this */ import Runner from "../lib/runner"
+import ScriptOptions from "../lib/script-options"
 
-import Runner from '../lib/runner';
-import ScriptOptions from '../lib/script-options';
-
-describe('Runner', () => {
+describe("Runner", () => {
   beforeEach(() => {
-    this.command = 'node';
-    this.runOptions = new ScriptOptions();
-    this.runOptions.cmd = this.command;
-    this.runner = new Runner(this.runOptions);
-  });
+    this.command = "node"
+    this.runOptions = new ScriptOptions()
+    this.runOptions.cmd = this.command
+    this.runner = new Runner(this.runOptions)
+  })
 
   afterEach(() => {
-    this.runner.destroy();
-  });
+    this.runner.destroy()
+  })
 
-  describe('run', () => {
-    it('with no input', () => {
+  describe("run", () => {
+    it("with no input", () => {
       runs(() => {
-        this.output = null;
+        this.output = null
         this.runner.onDidWriteToStdout((output) => {
-          this.output = output;
-        });
-        this.runner.run(this.command, ['./spec/fixtures/outputTest.js'], {});
-      });
+          this.output = output
+        })
+        this.runner.run(this.command, ["./spec/fixtures/outputTest.js"], {})
+      })
 
-      waitsFor(() => this.output !== null, 'File should execute', 2000);
+      waitsFor(() => this.output !== null, "File should execute", 2000)
 
-      runs(() => expect(this.output).toEqual({ message: 'hello\n' }));
-    });
+      runs(() => expect(this.output).toEqual({ message: "hello\n" }))
+    })
 
-    it('with an input string', () => {
+    it("with an input string", () => {
       runs(() => {
-        this.output = null;
+        this.output = null
         this.runner.onDidWriteToStdout((output) => {
-          this.output = output;
-        });
-        this.runner.run(this.command, ['./spec/fixtures/ioTest.js'], {}, 'hello');
-      });
+          this.output = output
+        })
+        this.runner.run(this.command, ["./spec/fixtures/ioTest.js"], {}, "hello")
+      })
 
-      waitsFor(() => this.output !== null, 'File should execute', 2000);
+      waitsFor(() => this.output !== null, "File should execute", 2000)
 
-      runs(() => expect(this.output).toEqual({ message: 'TEST: hello\n' }));
-    });
+      runs(() => expect(this.output).toEqual({ message: "TEST: hello\n" }))
+    })
 
-    it('exits', () => {
+    it("exits", () => {
       runs(() => {
-        this.exited = false;
+        this.exited = false
         this.runner.onDidExit(() => {
-          this.exited = true;
-        });
-        this.runner.run(this.command, ['./spec/fixtures/outputTest.js'], {});
-      });
+          this.exited = true
+        })
+        this.runner.run(this.command, ["./spec/fixtures/outputTest.js"], {})
+      })
 
-      waitsFor(() => this.exited, 'Should receive exit callback', 2000);
-    });
+      waitsFor(() => this.exited, "Should receive exit callback", 2000)
+    })
 
-    it('notifies about writing to stderr', () => {
+    it("notifies about writing to stderr", () => {
       runs(() => {
-        this.failedEvent = null;
+        this.failedEvent = null
         this.runner.onDidWriteToStderr((event) => {
-          this.failedEvent = event;
-        });
-        this.runner.run(this.command, ['./spec/fixtures/throw.js'], {});
-      });
+          this.failedEvent = event
+        })
+        this.runner.run(this.command, ["./spec/fixtures/throw.js"], {})
+      })
 
-      waitsFor(() => this.failedEvent, 'Should receive failure callback', 2000);
+      waitsFor(() => this.failedEvent, "Should receive failure callback", 2000)
 
-      runs(() => expect(this.failedEvent.message).toMatch(/kaboom/));
-    });
+      runs(() => expect(this.failedEvent.message).toMatch(/kaboom/))
+    })
 
-    it('terminates stdin', () => {
+    it("terminates stdin", () => {
       runs(() => {
-        this.output = null;
+        this.output = null
         this.runner.onDidWriteToStdout((output) => {
-          this.output = output;
-        });
-        this.runner.run(this.command, ['./spec/fixtures/stdinEndTest.js'], {}, 'unused input');
-      });
+          this.output = output
+        })
+        this.runner.run(this.command, ["./spec/fixtures/stdinEndTest.js"], {}, "unused input")
+      })
 
-      waitsFor(() => this.output !== null, 'File should execute', 2000);
+      waitsFor(() => this.output !== null, "File should execute", 2000)
 
-      runs(() => expect(this.output).toEqual({ message: 'stdin terminated\n' }));
-    });
-  });
-});
+      runs(() => expect(this.output).toEqual({ message: "stdin terminated\n" }))
+    })
+  })
+})
